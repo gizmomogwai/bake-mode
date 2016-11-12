2debe06878579da3bf6a582f37df2eaf7913ca22
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-11-12 19:31:15 +0100

    Added complete syntax
    hopefully

--- bake-mode.el
+++ bake-mode.el
@@ -18,7 +18,6 @@ Emacs adds spaces/tabs according to your settings."
   :tag "Indent"
   :safe t)
 
-
 (defun bake-indent-function ()
   "Indent a line for a bake file."
   (interactive)
@@ -27,9 +26,9 @@ Emacs adds spaces/tabs according to your settings."
     (let* ((p1 (line-beginning-position))
            (p2 (line-end-position))
            (l (s-trim (buffer-substring-no-properties p1 p2)))
-           (end-of-block (eq l "}"))
+           (end-of-block (string-match "}" l))
            (temporary-indent (car (syntax-ppss)))
-           (indent (if (string-match l "}") (1- temporary-indent) temporary-indent)))
+           (indent (if end-of-block (1- temporary-indent) temporary-indent)))
       ;;(message (format "syntax-ppss: temporary-indent: %s\nl: %s\nend-of-block: %s\nindent: %s" indent l end-of-block indent))
       (indent-line-to (* bake-mode/indent indent)))))
 
@@ -37,36 +36,84 @@ Emacs adds spaces/tabs according to your settings."
     'bake-mode
   '("#")
   '("Project"
+    "default"
+    "Description"
+    "RequiredBakeVersion"
+    "minimum"
+    "maximum"
+    "Responsible"
+    "Person"
+    "email"
     "ExecutableConfig"
-    "Set"
+    "LibraryConfig"
+    "CustomConfig"
+    "extends"
     "IncludeDir"
-    "DefaultToolchain"
-    "Compiler"
-    "CPP"
-    "ExternalLibrary"
-    "add"
-    "C"
-    "Linker"
-    "Flags"
-    "ArtifactName"
-    "Files"
-    "Dependency"
-    "Defines"
+    "inherit"
+    "true"
+    "false"
+    "inject"
+    "front"
+    "back"
+    "Set"
     "value"
+    "cmd"
+    "env"
+    "Dependency"
     "config"
+    "ExternalLibrary"
+    "search"
+    "UserLibrary"
+    "ExternalLibrarySearchPath"
+    "PreSteps"
+    "Makefile"
+    "lib"
+    "target"
+    "pathTo"
+    "filter"
+    "validExitCodes"
+    "Flags"
+    "CommandLine"
+    "on"
+    "off"
+    "filter"
+    "PostSteps"
+    "StartupSteps"
+    "ExitSteps"
+    "DefaultToolchain"
+    "outputDir"
+    "eclipseOrder"
+    "Compiler"
+    "ASM"
+    "CPP"
+    "C"
     "command"
-    "extends")
+    "Define"
+    "Archiver"
+    "Linker"
+    "LibPrefixFlags"
+    "LibPostfixFlags"
+    "LintPolicy"
+    "Docu"
+    "Prebuild"
+    "Except"
+    "Files"
+    "ExcludeFiles"
+    "ArtifactName"
+    "LinkerScript"
+    "MapFile"
   '(("=" . 'font-lock-operator)
     ("+" . 'font-lock-operator)
     (";" . 'font-lock-builtin)
+    ("add" . 'font-lock-constant-face)
+    ("remove" . 'font-lock-constant-face)
     ("[A-Za-z0-9]*" . 'font-lock-variable-name-face)
     )
   '("Project.meta")
   (list
    (lambda() (set (make-local-variable 'indent-line-function) #'bake-indent-function))
-
    )
-  "A mode for bakes Project.meta files"
+  "A mode for bake's Project.meta files"
   )
 
 (provide 'bake-mode)
